5113099d73cb7455a31f4808768cb929757cbfb4
Zekarias Negassa <zakfena@gmail.com>
2020-12-04 01:11:03 -0600

    touch ups

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
-import About from './pages/About';
-// import Contact from './pages/Contact';
-import Projects from './pages/Projects';
-import Header from './components/Header'
-
+import "./App.css";
+import About from "./pages/About";
+import projects from "./projects.json";
+import Card from "./components/ProjectCard";
+import Resume from "./pages/Resume";
+import Project from "./pages/Projects";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <Router>
-<main>
-      <Header />
-      <Route exact path="/" component={About} />
-      <Route exact path="/about" component={About} />
-      {/* <Route exact path="/contact" component={Contact} /> */}
-      <Route exact path="/projects" component={Projects} />
+      <div>
+        <Navbar />
 
-    </main>
-      </Router>
-  );  
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/projects" component={Project} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -1,60 +1,58 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './Header.css'
+import "./Header.css";
 function Header() {
+  const location = useLocation();
+
   return (
-    <header>
-      {/* <nav className="navbar navbar-dark bg-dark">
-  <!-- Navbar content -->
-</nav> */}
+    <ul className="nav nav-tabs">
+      
+      <li className="nav-item active">
+        <Link
+          to="/"
+          className={
+            location.pathname === "/" || location.pathname === "/about"
+              ? "nav-link active"
+              : "nav-link"
+          }
+        >
+          <h3>About</h3>
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/projects"
+          className={
+            location.pathname === "/projects" ? "nav-link active" : "nav-link"
+          }
+        >
+          <h3>Projects</h3>
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/resume"
+          className={
+            location.pathname === "/resume" ? "nav-link active" : "nav-link"
+          }
+        >
+          <h3>Resume</h3>
+        </Link>
+      </li>
+    </ul>
+    //     <header>
+    //       {/* <nav className="navbar navbar-dark bg-dark">
+    //   <!-- Navbar content -->
+    // </nav> */}
 
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <img className="logo" src="/my-logo.png" alt="logo" />
+    //       <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
-        <div className="" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <Link
-                to="/"
-                className={
-                  window.location.pathname === "/" ||
-                  window.location.pathname === "/about"
-                    ? "nav-link active"
-                    : "nav-link"
-                }
-              >
-                <h3>About</h3>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/project"
-                className={
-                  window.location.pathname === "/project"
-                    ? "nav-link active"
-                    : "nav-link"
-                }
-              >
-                Project
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/contact"
-                className={
-                  window.location.pathname === "/contact"
-                    ? "nav-link active"
-                    : "nav-link"
-                }
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+    //         <div className="" id="navbarNav">
+
+    //         </div>
+    //       </nav>
+    //     </header>
   );
 }
 
